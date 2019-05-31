890166bf9b1d7e6f2aa84ad3dd67eb61179187ff
leonard <leonard.francis@netapp.com>
2019-05-31 12:23:58 +0000

    NFSAAS-2316 update to R4 RP standard
    R4 standard of RP for GA
    Including changes due to GA command review comments.

--- src/command_modules/azure-cli-netappfiles/azure/cli/command_modules/netappfiles/tests/latest/test_volume_commands.py
+++ src/command_modules/azure-cli-netappfiles/azure/cli/command_modules/netappfiles/tests/latest/test_volume_commands.py
@@ -128,14 +128,14 @@ class AzureNetAppFilesVolumeServiceScenarioTest(ScenarioTest):
         assert vol_with_export_policy['name'] == account_name + '/' + pool_name + '/' + volume_name
         assert vol_with_export_policy['exportPolicy']['rules'][0]['allowedClients'] == '1.2.3.0/24'
         assert vol_with_export_policy['exportPolicy']['rules'][0]['ruleIndex'] == 3
-        assert vol_with_export_policy['exportPolicy']['rules'][0]['cifs'] == False
+        assert vol_with_export_policy['exportPolicy']['rules'][0]['cifs'] is False
 
         # and add another export policy
         vol_with_export_policy = self.cmd("netappfiles volume export-policy add -g {rg} -a %s -p %s -v %s --allowed-clients '1.2.4.0/24' --rule-index 2 --unix-read-only true --unix-read-write false --cifs true --nfsv3 true --nfsv4 false" % (account_name, pool_name, volume_name)).get_output_in_json()
         assert vol_with_export_policy['name'] == account_name + '/' + pool_name + '/' + volume_name
         assert vol_with_export_policy['exportPolicy']['rules'][1]['allowedClients'] == '1.2.3.0/24'
         assert vol_with_export_policy['exportPolicy']['rules'][0]['allowedClients'] == '1.2.4.0/24'
-        assert vol_with_export_policy['exportPolicy']['rules'][0]['cifs'] == True
+        assert vol_with_export_policy['exportPolicy']['rules'][0]['cifs'] is True
         assert len(vol_with_export_policy['exportPolicy']['rules']) == 3
 
         # list the policies
